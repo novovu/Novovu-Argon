3dd67f457fb8118c16501bc364ae814c613120d9
Jay <jay.mann244@gmail.com>
2019-10-03 22:06:24 -0500

    Created strong framework

---
+++ Novovu.Argon.Testbench/Main.cs
@@ -0,0 +1,42 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Novovu.Argon.Testbench
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            InitializeChromium();
+        }
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+            // Create a browser component
+            chromeBrowser = new ChromiumWebBrowser("https://novovu.com");
+            // Add it to the form and fill it to the form window.
+            this.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+        }
+        public ChromiumWebBrowser chromeBrowser;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+    }
+}
